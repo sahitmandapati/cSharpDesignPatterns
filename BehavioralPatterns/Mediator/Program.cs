6713ebf7865fef526a3a3134aba4dceddf4251d6
sahitmandapati <sahit.m@technovert.com>
2024-03-20 18:17:32 +0530

    add mediator and observer pattern

---
+++ BehavioralPatterns/Mediator/Program.cs
@@ -0,0 +1,106 @@
+﻿// Mediator pattern.
+
+// The Mediator pattern is a behavioral design pattern that encapsulates how a set of objects interact. It promotes loose coupling by keeping objects from referring to each other explicitly, and it allows their interaction to vary independently. Here are the terminologies used in the Mediator pattern:
+
+// Mediator: This is an interface that defines a contract for communication between colleague objects. In this case, it has a method SendOrder which is used to send orders.
+
+// Concrete Mediator: This is a class that implements the Mediator interface. It coordinates communication between colleague objects. It maintains a list of PizzaBase objects and can send orders to them.
+
+// Colleague: These are the classes that communicate with each other via the Mediator. In this case, PizzaBase is an abstract class that represents a pizza base. It has a reference to the Mediator object and a method to order a pizza.
+
+// Concrete Colleague: These are specific implementations of the Colleague class. They use the Mediator to communicate with other colleagues.
+
+// Client: This is where the Mediator and Colleagues are instantiated and used. The client configures the system by creating a Mediator, creating Colleagues, and registering the Colleagues with the Mediator.
+
+// We use Mediator pattern when the communication between objects is complex and needs to be centralized. It also helps in reducing the complexity of communication between objects.
+
+// In this program we will create a pizza using the Mediator pattern.
+
+// Mediator : IPizzaOrderMediator
+// Concrete Mediator : PizzaOrderMediator
+// Colleague : PizzaBase
+// Concrete Colleague : ThinCrustPizza, ThickCrustPizza
+// Client : Program
+
+// Mediator interface
+interface IPizzaOrderMediator
+{
+    void SendOrder(string order, PizzaBase pizzaBase);
+}
+
+// Concrete Mediator
+class PizzaOrderMediator : IPizzaOrderMediator
+{
+    private List<PizzaBase> pizzaBases = new List<PizzaBase>();
+
+    public void AddPizzaBase(PizzaBase pizzaBase)
+    {
+        pizzaBases.Add(pizzaBase);
+    }
+
+    public void SendOrder(string order, PizzaBase pizzaBase)
+    {
+        Console.WriteLine($"Order received: {order}");
+        Console.WriteLine($"Preparing pizza with {pizzaBase.Type} base");
+        pizzaBase.CompleteOrder();
+    }
+}
+
+// Colleague
+abstract class PizzaBase
+{
+    protected IPizzaOrderMediator mediator;
+    public string Type { get; }
+
+    public PizzaBase(IPizzaOrderMediator mediator, string type)
+    {
+        this.mediator = mediator;
+        this.Type = type;
+    }
+
+    public abstract void OrderPizza();
+    public void CompleteOrder()
+    {
+        Console.WriteLine($"Pizza with {Type} base is ready!");
+    }
+}
+
+// Concrete Colleague
+class ThinCrustPizza : PizzaBase
+{
+    public ThinCrustPizza(IPizzaOrderMediator mediator) : base(mediator, "thin crust") { }
+
+    public override void OrderPizza()
+    {
+        mediator.SendOrder("Thin crust pizza", this);
+    }
+}
+
+// Concrete Colleague
+class ThickCrustPizza : PizzaBase
+{
+    public ThickCrustPizza(IPizzaOrderMediator mediator) : base(mediator, "thick crust") { }
+
+    public override void OrderPizza()
+    {
+        mediator.SendOrder("Thick crust pizza", this);
+    }
+}
+
+// Client
+class Program
+{
+    static void Main(string[] args)
+    {
+        var mediator = new PizzaOrderMediator();
+
+        var thinCrustPizza = new ThinCrustPizza(mediator);
+        var thickCrustPizza = new ThickCrustPizza(mediator);
+
+        mediator.AddPizzaBase(thinCrustPizza);
+        mediator.AddPizzaBase(thickCrustPizza);
+
+        thinCrustPizza.OrderPizza();
+        thickCrustPizza.OrderPizza();
+    }
+}
---
+++ BehavioralPatterns/Observer/Program.cs
@@ -0,0 +1,109 @@
+﻿// Observer Pattern
+
+// The Observer pattern is a behavioral design pattern that allows an object (the subject) to publish changes to its state. Other objects (the observers) subscribe to be immediately notified of any changes. This pattern promotes loose coupling between the objects, which means that the subject doesn't need to know anything about the observers and vice versa.
+
+// Here are the terminologies used in the Observer pattern:
+
+// Subject: This is an interface that defines the contract for the subject. It has methods to register, remove, and notify observers.
+
+// Concrete Subject: This is a class that implements the Subject interface. It maintains a list of observers and notifies them of any changes in its state.
+
+// Observer: This is an interface that defines the contract for the observer. It has a method to update the observer when the subject's state changes.
+
+// Concrete Observer: This is a class that implements the Observer interface. It registers itself with the subject and gets notified when the subject's state changes.
+
+// Client: This is where the subject and observers are instantiated and used. The client configures the system by creating a subject, creating observers, and registering the observers with the subject.
+
+// We use the Observer pattern when we want to notify multiple objects about changes in the state of another object. It also helps in reducing the coupling between the subject and the observers.
+
+// In this program, we will create a pizza using the Observer pattern.
+
+// Subject: IPizzaOrder
+// Concrete Subject: PizzaOrder
+// Observer: IOrderObserver
+// Concrete Observer: PizzaBase, PizzaTopping
+// Client: Program
+
+// Subject interface
+interface IPizzaOrder
+{
+    void AddObserver(IOrderObserver observer);
+    void RemoveObserver(IOrderObserver observer);
+    void NotifyObservers();
+}
+
+
+// Concrete Subject
+
+class PizzaOrder : IPizzaOrder
+{
+    private List<IOrderObserver> observers = new List<IOrderObserver>();
+    private string order;
+
+    public void AddObserver(IOrderObserver observer)
+    {
+        observers.Add(observer);
+    }
+
+    public void RemoveObserver(IOrderObserver observer)
+    {
+        observers.Remove(observer);
+    }
+
+    public void NotifyObservers()
+    {
+        foreach (var observer in observers)
+        {
+            observer.Update(order);
+        }
+    }
+
+    public void SetOrder(string order)
+    {
+        this.order = order;
+        NotifyObservers();
+    }
+}
+
+// Observer interface
+
+interface IOrderObserver
+{
+    void Update(string order);
+}
+
+// Concrete Observer
+
+class PizzaBase : IOrderObserver
+{
+    public void Update(string order)
+    {
+        Console.WriteLine($"Preparing pizza with {order} base");
+    }
+}
+
+// Concrete Observer
+
+class PizzaTopping : IOrderObserver
+{
+    public void Update(string order)
+    {
+        Console.WriteLine($"Adding {order} topping to the pizza");
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        PizzaOrder pizzaOrder = new PizzaOrder();
+        PizzaBase pizzaBase = new PizzaBase();
+        PizzaTopping pizzaTopping = new PizzaTopping();
+
+        pizzaOrder.AddObserver(pizzaBase);
+        pizzaOrder.AddObserver(pizzaTopping);
+
+        pizzaOrder.SetOrder("thin crust");
+    }
+}
